a64ec5d96090bc8ce99c507c09443d31bc0e2641
Dispy-Mac <tpps89305@hotmail.com>
2023-04-17 17:54:58 +0800

    Build home page

---
+++ iosCJDict/iosCJDict/CangDictTile.swift
@@ -0,0 +1,44 @@
+//
+//  CangDictTile.swift
+//  iosCJDict
+//
+//  Created by 楊朝富 on 2023/4/17.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct CangDictTile: View {
+    let word: String
+    let root: String
+    let letter: String
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(Color("Primary"))
+            HStack(alignment: .bottom) {
+                Text(word)
+                    .font(.system(size: 28))
+                    .foregroundColor(Color.white)
+                HStack {
+                    Text(root)
+                        .foregroundColor(Color.white)
+                    Text(letter)
+                        .foregroundColor(Color.white)
+                }
+                .padding(.bottom, 2.0)
+                
+                Spacer()
+            }
+            .padding(.all)
+        }
+    }
+}
+
+struct CangDictTile_Previews: PreviewProvider {
+    static var previews: some View {
+        CangDictTile(word: "安", root: "十女", letter: "JV")
+            .background(Color.gray)
+    }
+}
--- iosCJDict/iosCJDict/ContentView.swift
+++ iosCJDict/iosCJDict/ContentView.swift
@@ -3,28 +3,58 @@ import cjdict
 
 struct ContentView: View {
     @State var input = ""
-    @State var result = ""
     var cangJi5 = CangJi5Dict()
+    @State var testArray: [CangWord] = []
+    
+    init() {
+        let coloredAppearance = UINavigationBarAppearance()
+        coloredAppearance.backgroundColor = UIColor(named: "Primary")
+        coloredAppearance.titleTextAttributes = [
+            .foregroundColor: UIColor.white,
+            .font: UIFont.systemFont(ofSize: 20)
+        ]
+        UINavigationBar.appearance().standardAppearance = coloredAppearance
+        UINavigationBar.appearance().compactAppearance = coloredAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = coloredAppearance
+    }
 
-	var body: some View {
-        VStack {
-            HStack {
+    var body: some View {
+        NavigationView {
+            VStack(spacing: 0.0) {
                 TextField("輸入欲查詢的字", text: $input)
-                Button("查詢") {
-                    var array = cangJi5.getCangJiCode(words: input) as! [CangWord]
-                    if (!array.isEmpty) {
-                        self.result = array[0].root
-                    }
+                    .padding(16.0)
+                    .onChange(of: input, perform: { newValue in
+                        let array = cangJi5.getCangJiCode(words: newValue) as! [CangWord]
+                        if (!array.isEmpty) {
+                            self.testArray = array
+                        } else {
+                            self.testArray.removeAll()
+                        }
+                    })
+                Rectangle()
+                    .fill(Color.gray)
+                    .frame(height: 1.0)
+                List(testArray.indices, id: \.self) { index in
+                    CangDictTile(
+                        word: testArray[index].word,
+                        root: testArray[index].root,
+                        letter: testArray[index].letter
+                    )
+                    .listRowInsets(EdgeInsets())
                 }
+                .padding(.all)
+                .listStyle(.plain)
+                Spacer()
             }
-            Text(result)
+            
+            .navigationTitle("倉頡字典")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding(10.0)
-	}
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
 	static var previews: some View {
-		ContentView()
+        ContentView()
 	}
 }
